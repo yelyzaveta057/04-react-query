4d371c0442363f4c2dcbfe5a86d70c73043c99c2
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-06-09 00:46:16 -0400

    Update movieService.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,19 +3,28 @@ import type { Movie } from '../types/movie';
 
 const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 
+
+
 export interface MoviesHttpResponse {
   results: Movie[];
+    total_pages: number;
 }
 
 export const fetchMovies = async (
-  query: string
-): Promise<Movie[]> => {
-    console.log("API_KEY:", API_KEY);
+  query: string,
+  page = 1
+): Promise<MoviesHttpResponse> => {
+  console.log("API_KEY:", API_KEY);
 
   const response = await axios.get<MoviesHttpResponse>(
     'https://api.themoviedb.org/3/search/movie',
     {
-      params: { query, language: 'en-US', include_adult: false },
+      params: {
+        query,
+        language: 'en-US',
+        include_adult: false,
+        page,
+      },
       headers: {
         Authorization: `Bearer ${API_KEY}`,
         Accept: 'application/json',
@@ -23,5 +32,8 @@ export const fetchMovies = async (
     }
   );
 
-  return response.data.results;
+
+  return response.data;
+ 
+
 };
