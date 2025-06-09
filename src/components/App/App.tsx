b3bd65681f79df043ee6212001f79ccdc0fa64ca
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-06-09 00:46:14 -0400

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 import type { Movie } from "../../types/movie";
@@ -11,38 +11,55 @@ import Loader from "../Loader/Loader";
 import MovieModal from "../MovieModal/MovieModal";
 
 import styles from "./App.module.css";
+import ReactPaginate from 'react-paginate';
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [movies, setMovies] = useState<Movie[]>([]);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handleSelectMovie = (movie: Movie) => setSelectedMovie(movie);
 
   const closeModal = () => setSelectedMovie(null);
 
-  const handleSearch = async (query: string) => {
-    setMovies([]);
-    setIsError(false);
-    setIsLoading(true);
-
-    try {
-      const data = await fetchMovies(query); // data — це вже масив Movie[]
-
-      if (data.length === 0) {
-        toast.error("No movies found for your request.");
-        return;
-      }
-
-      setMovies(data);
-    } catch {
-      toast.error("Something went wrong.");
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = async (newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
   };
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      if (!query) return;
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const { results, total_pages } = await fetchMovies(query, page);
+
+
+
+        if (results.length === 0) {
+          toast.error("No movies found for your request.");
+          return;
+        }
+        setMovies(results);
+        setTotalPages(total_pages);
+      } catch {
+        toast.error("Something went wrong.");
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadMovies();
+  }, [query, page]);
+
+
 
   return (
     <div className={styles.app}>
@@ -53,7 +70,22 @@ export default function App() {
       {isError && <ErrorMessage />}
 
       {movies.length > 0 && (
-        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+        <>
+          <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+          {totalPages > 1 && (
+            <ReactPaginate
+              pageCount={totalPages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={styles.pagination}
+              activeClassName={styles.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+        </>
       )}
 
       {selectedMovie && (
@@ -61,4 +93,6 @@ export default function App() {
       )}
     </div>
   );
+  
+
 }
