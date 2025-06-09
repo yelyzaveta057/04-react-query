50b4c90959bf0323aade2d004948ef8319bebbfa
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-06-09 10:47:58 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App"; 
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const queryClient = new QueryClient();
+
+import App from "./components/App/App.tsx";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
